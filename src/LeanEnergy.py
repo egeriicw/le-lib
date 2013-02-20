c98bad857ee2f9858ff17fd93546a833aa42e1de
Bill Eger <egeriicw@gmail.com>
2013-02-20 17:44:58 -0500

    added number of runs for sliding NAC

--- src/LeanEnergy.py
+++ src/LeanEnergy.py
@@ -18,91 +18,43 @@ def threeP(y, x,  div):
     dx = (xMax - xMin) / div
     print "dx: ", dx
     
-    #np.linespace(start, stop, number of samples, include endpoint or not)
-
-    #mult = np.linspace(1,  div,  num=div,  endpoint=True)
-    mult = np.linspace(1,  div,  num=(div-1),  endpoint=False)
+    mult = np.linspace(1,  div,  div)
     
     r2 = np.zeros(len(x))
-    adjr2 = np.zeros(len(x))
-    rmse = np.zeros(len(x))
-    cvrmse = np.zeros(len(x))
     yHat = np.zeros(len(x))
 
     # First Pass
     
     for m in mult:
-
-        xTemp = x
-
-        # Calculate various change point temperatures to test.  Not sure if I should include m or (m-1)?  Will need to research this further.  If including m, then need to set "endpoint = True" in "np.linspace" above.
-        xCp = xMin + m*dx
-        # xCp = xMin + (m-1)*dx
-
-        #  THis is where we would test for 3PC or 3PH models using a control statement.  i.e. If 3PH then do ...  elseif 3PC then do...
-        #  Depending on the choice, you would alter the way that xCp for 0 terms.  For now, we are only considering 3PC.
-
-        xTemp = xTemp - xCp
+        xTemp = x - (xMin + (m-1)*dx)
         xTemp[xTemp <= 0.] = 0.
-        
-        print "Round: ",  m
-        print "xCp: ",  xCp
-        print "xTemp: ",  xTemp
-        
-        # In the future, this is where I would add additional x variables for multivariate analysis.  May need a control structure
-        # to manage numpy and scipy implementations of OLS regression.  As of current, it is strictly an univariate OLS regression.
-        
+        print "round: ", m
+        print "xTemp: ", xTemp
         A = np.vstack([xTemp,  np.ones(len(xTemp))]).T
 
         result1 = np.linalg.lstsq(A, y)
 
-        # Need to calculate RMSE, CV-RMSE, r2, adjR2, sigma, roe
-        # yHat[np.where(xTemp == 0.)] = np.mean(y[np.where(xTemp == 0.)])
-        # yHat[np.where(xTemp != 0.)] = result1[0][0]*xTemp[np.where(xTemp != 0.)] + result1[0][1]
-        # ssm:  np.sum(np.square(y - np.mean(y)))
-        # sse: np.sum(np.square(y - yHat))
-        # mse (kelly): sse / (number of rows - number of columns)
-        # mse (wikipedia): np.sum(np.square(yHat - y)) / number of rows
-        # r2:  1 - sse / ssm
-        # rmse: np.sqrt(mse)
-        # cv-rmse: rmse / ymean * 100
-
         yHat[np.where(xTemp == 0.)] = np.mean(y[np.where(xTemp == 0.)])
         yHat[np.where(xTemp != 0.)] = result1[0][0]*xTemp[np.where(xTemp != 0.)] + result1[0][1]
-         
-        ssm = np.sum(np.square(y - np.mean(y)))
-        sse = np.sum(np.square(y-yHat))
-        mse = np.sum(np.square(yHat - y)) / len(y)
-        rmse[m] = np.sqrt(mse)
-        cvrmse[m] = rmse[m] / np.mean(y)
-        r2[m] = 1 - sse/ssm
-        adjr2[m] = 1 - (len(y) - 1) / (len(y) - (A.shape[1] -1))
-     
+        
         print "yHat: ", yHat
-        print "rmse: ",  rmse[m]
-        print "cv-rmse: ",  cvrmse[m]
-        print "r2: ",  r2[m]
-        print "adj-R2: ",  adjr2[m]
-
-        # Now need to select where rmse is lowest and use that as the starting point for the next iteration of the grid search.
-
-        plot(x,  y,  yHat)
-        
-        """
-        #print "r2: ", r2 = 1 - sse / ssm
-        
-        
+        print "r2: ", 1 - (np.sum(np.square(y - yHat)) / np.sum(np.square(y - np.mean(y))))
         
         r2[m] = rsquared(result1[1],  y)
 
     print "m: ", result1[0][0]
     print "b: ", result1[0][1]
+
+    plt.plot(x, y, 'o')
+    plt.plot(x, yHat, 'r')
+    plt.show()
+
     
     #print "r2: ",  r2
 
     mIndex = np.mean(np.median(np.where(r2 == np.max(r2))))
 
-    
+    """
     
     # Second Pass
 
@@ -136,17 +88,15 @@ def threeP(y, x,  div):
     
     #print "r2: ",  r2
     #print "b3: ", xMin + np.mean(np.median(np.where(r2 == np.max(r2))))*dx
-    
     """
-    
 def rsquared(r,  y):
     return 1 - r/(y.size * y.var())
 
-def plot(x,  y,  yhat):
-    plt.plot(x, y, 'o')
-    plt.plot(x, yhat, 'r')
-    plt.show()
-    
+
+def slidingThreeP(x, y):
+    # Number of runs for monthly data; need to write an algorithm to detect whether hourly, daily, monthly.
+    runs = x.length - 12
+     
 def main():
     print "Main"
 
